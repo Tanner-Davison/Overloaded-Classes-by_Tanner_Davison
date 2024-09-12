947e91acd28a226bbf6cbf42bff93e51b71ff960
Tanner Davison <tanner.davison95@gmail.com>
2024-09-12 11:11:13 -0600

    stashing

---
+++ Class_Overloading_by_Tanner_Davison/Alien.h
@@ -0,0 +1,16 @@
+//
+// Created by Tanner Davison on 9/8/24.
+//
+
+#ifndef ALIEN_H
+#define ALIEN_H
+
+
+
+class Alien {
+
+};
+
+
+
+#endif //ALIEN_H
---
+++ Class_Overloading_by_Tanner_Davison/IBaseInterface.h
@@ -0,0 +1,16 @@
+//
+// Created by Tanner Davison on 9/8/24.
+//
+
+#ifndef IBASEINTERFACE_H
+#define IBASEINTERFACE_H
+
+
+
+class IBaseInterface {
+
+};
+
+
+
+#endif //IBASEINTERFACE_H
