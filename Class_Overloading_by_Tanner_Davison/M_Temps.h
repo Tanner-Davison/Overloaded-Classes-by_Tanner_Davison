b03223930f4d08ccb94216f6152e69a956b9ed2d
Tanner Davison <Tboydavison@gmail.com>
2024-09-13 00:16:46 -0600

    fixed M_Temps.h

--- Class_Overloading_by_Tanner_Davison/M_Temps.h
+++ Class_Overloading_by_Tanner_Davison/M_Temps.h
@@ -25,8 +25,8 @@ template <typename T>
 void CharClass<T>::setMember(const T& memberP) {
 	this->member = memberP;
 }
-template <typename T>
-inline std::ostream& operator<<(std::ostream& output, const CharClass<T>& charclass) {
+template <typename B>
+inline std::ostream& operator<<(std::ostream& output, const CharClass<B>& charclass) {
 	output << charclass.getMember();
 
 	return output;
@@ -69,3 +69,71 @@ public:
 private:
 	vector<T> names;
 };
+
+template <typename T>
+
+std::vector<T> addTwoArrays(const std::vector<T>& arrOne, const std::vector<T>& arrTwo) {
+	size_t _SIZE = (arrOne.size() > arrTwo.size()) ? arrOne.size() : arrTwo.size();
+	std::vector<T> temp(_SIZE);
+	for (int i = 0; i < _SIZE; i++) {
+		if (i >= arrOne.size()) {
+			temp[i] = arrTwo[i];
+		}
+		else if (i >= arrTwo.size()) {
+			temp[i] = arrOne[i];
+		}
+		else {
+			temp[i] = (arrOne[i] + arrTwo[i]);
+		}
+	}
+	return temp;
+}
+template <typename T = int, typename T1 = int>
+struct Pairs {
+	T first;
+	T second;
+	std::vector<T> vec1;
+	std::vector<T1> vec2;
+	~Pairs() = default;
+	Pairs operator+(const Pairs<T>& other) const {
+		std::vector<T> arrVecOne = addTwoArrays(this->vec1, other.vec1);
+		vector<T1> arrVecTwo = addTwoArrays(this->vec2, other.vec2);
+
+		return Pairs{ this->first + other.first, this->second + other.second, arrVecOne, arrVecTwo };
+	}
+};
+template <typename T>
+T addMembers(const Pairs<T>& pairsP) {
+	return(pairsP.first + pairsP.second);
+}
+template <typename T>
+std::ostream& operator<<(std::ostream& output, const std::vector<T> arr) {
+	for (T val : arr) {
+		output << val << ", ";
+	}
+	std::cout << "\n";
+	return output;
+}
+static inline std::ostream& operator<<(std::ostream& output, const std::vector<std::string>& arr) {
+	for (std::string val : arr) {
+		output << "[ " << val << " ], ";
+	}
+	std::cout << "\n";
+	return output;
+}
+template <typename T>
+static inline std::ostream& operator<<(std::ostream& output, const Pairs<T> pairP) {
+	output << "First: " << pairP.first << " Second: " << pairP.second << std::endl;
+	return output;
+}
+
+template <typename T>
+T smallestMembers(const Pairs<T>& pairP) {
+	return(pairP.first < pairP.second ? pairP.first : pairP.second);
+}
+template <typename T>
+T smallestMembers(const Pairs<T>* pairP) {
+	return (pairP.first < pairP.second ? pairP.first : pairP.second);
+}
+
+
--- Class_Overloading_by_Tanner_Davison/main.cpp
+++ Class_Overloading_by_Tanner_Davison/main.cpp
@@ -5,72 +5,8 @@
 #include "IBaseInterface.h"
 #include "InterfaceDerived.h"
 #include "ExampleTemplate.h"
+#include "M_Temps.h"
 
-template <typename T>
-
-std::vector<T> addTwoArrays(const std::vector<T>& arrOne, const std::vector<T>& arrTwo) {
-	size_t _SIZE = (arrOne.size() > arrTwo.size()) ? arrOne.size() : arrTwo.size();
-	std::vector<T> temp(_SIZE);
-	for (int i = 0; i < _SIZE; i++) {
-		if (i >= arrOne.size()) {
-			temp[i] = arrTwo[i];
-		}
-		else if (i >= arrTwo.size()) {
-			temp[i] = arrOne[i];
-		}
-		else {
-			temp[i] = (arrOne[i] + arrTwo[i]);
-		}
-	}
-	return temp;
-}
-template <typename T = int, typename T1 = int>
-struct Pairs {
-	T first;
-	T second;
-	std::vector<T> vec1;
-	std::vector<T1> vec2;
-	~Pairs() = default;
-	Pairs operator+(const Pairs<T>& other) const {
-		std::vector<T> arrVecOne = addTwoArrays(this->vec1, other.vec1);
-		vector<T1> arrVecTwo = addTwoArrays(this->vec2, other.vec2);
-
-		return Pairs{ this->first + other.first, this->second + other.second, arrVecOne, arrVecTwo };
-	}
-};
-template <typename T>
-T addMembers(const Pairs<T>& pairsP) {
-	return(pairsP.first + pairsP.second);
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& output, const std::vector<T> arr) {
-	for (T val : arr) {
-		output << val << ", ";
-	}
-	std::cout << "\n";
-	return output;
-}
-std::ostream& operator<<(std::ostream& output, std::vector<std::string>& arr) {
-	for (std::string val : arr) {
-		output << "[ " << val << " ], ";
-	}
-	std::cout << "\n";
-	return output;
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& output, Pairs<T> pairP) {
-	output << "First: " << pairP.first << " Second: " << pairP.second << std::endl;
-	return output;
-}
-
-template <typename T>
-T smallestMembers(const Pairs<T>& pairP) {
-	return(pairP.first < pairP.second ? pairP.first : pairP.second);
-}
-template <typename T>
-T smallestMembers(const Pairs<T>* pairP) {
-	return (pairP.first < pairP.second ? pairP.first : pairP.second);
-}
 int main() {
 
 	Pairs<int, int> myPairs{ 100,200 };
