84068c2289b6b6a721075944431bc5b16c9ef6a6
Tanner Davison <Tboydavison@gmail.com>
2024-09-13 01:55:12 -0600

    Using macros to print int defs and boolalphas also input stream parsing;

--- Class_Overloading_by_Tanner_Davison/main.cpp
+++ Class_Overloading_by_Tanner_Davison/main.cpp
@@ -7,16 +7,20 @@
 #include "ExampleTemplate.h"
 #include "M_Temps.h"
 #include "Friend_Class_Templates.h"
-
+#include <sstream>
 
 int main() {
 
-	int x = 44;
-	EntityEx<int*> entity{ &x };
-	toStrings(entity);
-	cout << entity;
+	std::cout << "MAX VALUE FOR INT: " << std::numeric_limits<int>::max() << std::endl;
+	std::cout << "MAX LIMIT OF INT: " << INT_MAX << std::endl;
 
-
+	std::cout << std::boolalpha;
+	std::cout << "Is INT signed? " << std::numeric_limits<int>::is_signed << std::endl;
+	bool b1, b2;
+	std::istringstream is("true false");
+	is >> std::boolalpha >> b1 >> b2;
+	std::cout << '"' << is.str() << "\" parsed as: "
+		<< std::boolalpha << b1 << ' ' << b2 << '\n';
 	std::cout << "\n--------END OF PROGRAM--------" << endl;
 
 	return 1;
